895bbfbf00279154a3261d928c981b9d0d8a3b72
root <root@Node>
2023-11-08 03:20:26 -0800

    Notion initialization and marketing Page implementatiun

---
+++ app/(Marketing)/_components/navbar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+// import { useConvexAuth } from "convex/react";
+// import { SignInButton, UserButton } from "@clerk/clerk-react";
+// import Link from "next/link";
+
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Button } from "@/components/ui/button";
+// import { Spinner } from "@/components/spinner";
+import { cn } from "@/lib/utils";
+
+import { Logo } from "./logo";
+
+export const Navbar = () => {
+  // const { isAuthenticated, isLoading } = useConvexAuth();
+  const scrolled = useScrollTop();
+
+  return (
+    <div 
+    className={cn(
+      "z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6",
+      scrolled && "border-b shadow-sm"
+    )}>
+      <Logo />
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        <ModeToggle />
+      </div>
+    </div>
+  )
+}
